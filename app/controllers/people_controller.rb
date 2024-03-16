7cd59fb30fc5071ab3f9ad80669cf4b3eff7bb84
Mayhm <nisekoimylove@gmail.com>
2024-03-15 22:04:25 -0300

    refactor: apply will_paginate

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -15,7 +15,7 @@ class PeopleController < ApplicationController
       @active = true
     end
 
-    @people = Person.where(active: @active).paginate(page: params[:page], per_page: 100)
+    @people = Person.where(active: @active).paginate(page: params[:page], per_page: 30)
   end
 
   # GET /people/search?q=a_name
